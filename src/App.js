a4ff5981649833fc89837b2842208cf14ce82dd9
MrNihalT <nihal.chiyoor@gmail.com>
2025-01-08 21:29:08 +0530

    Fix routing issue and update deployment

--- src/App.js
+++ src/App.js
@@ -5,41 +5,40 @@ import Place from "./components/screens/Place";
 import NotFound from "./components/screens/NotFound";
 import Login from "./components/screens/Login";
 import Signup from "./components/screens/Signup";
-import React, {useState , useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 export const UserContext = React.createContext();
 
 function App(props) {
-    const [userData,setUserData] = useState({});
-    const updateUserdata = (action) =>{
-        switch(action.type){
-            case "LOGOUT" :
+    const [userData, setUserData] = useState({});
+    const updateUserdata = (action) => {
+        switch (action.type) {
+            case "LOGOUT":
                 setUserData(null);
                 localStorage.clear();
                 break;
-                case "LOGIN":
-                    setUserData(action.payload);
-                    break;
+            case "LOGIN":
+                setUserData(action.payload);
+                break;
             default:
                 break;
         }
-    }
-    useEffect(()=>{
-       setUserData(JSON.parse(localStorage.getItem("user_data")));
-        
-    },[]);
+    };
+    useEffect(() => {
+        setUserData(JSON.parse(localStorage.getItem("user_data")));
+    }, []);
     return (
         <div>
-            <UserContext.Provider value={userData,updateUserdata}>
-            <Router>
-                <Switch>
-                    <Route path="/" exact component={Places} />
-                    <Route path="/auth/login/" exact component={Login} />
-                    <Route path="/auth/create/" exact component={Signup} />
-                    <Route path="/place/:id" component={Place} />
-                    <Route component={NotFound} />
-                </Switch>
-            </Router>
+            <UserContext.Provider value={(userData, updateUserdata)}>
+                <Router basename="/Traveller-App">
+                    <Switch>
+                        <Route path="/" exact component={Places} />
+                        <Route path="/auth/login/" exact component={Login} />
+                        <Route path="/auth/create/" exact component={Signup} />
+                        <Route path="/place/:id" component={Place} />
+                        <Route component={NotFound} />
+                    </Switch>
+                </Router>
             </UserContext.Provider>
         </div>
     );
